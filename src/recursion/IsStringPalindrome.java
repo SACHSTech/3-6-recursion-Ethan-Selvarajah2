6fea38ee04e705c936a496aaf36e64f158bc707f
Ethan-Selvarajah2 <ethan.selvarajah25@ycdsbk12.ca>
2025-05-16 23:05:37 -0400

    Palindrome done

--- src/recursion/IsStringPalindrome.java
+++ src/recursion/IsStringPalindrome.java
@@ -2,10 +2,15 @@ package recursion;
 
 public class IsStringPalindrome {
     public static boolean isPalindrome(String s) {
-        if (s.length() == 1) {
+        if (s.length() <= 1) {
             return true;
         }
 
+        if (s.charAt(0) != s.charAt(s.length() - 1)){
+            return false;
+        }
+
+        return isPalindrome(s.substring(1, s.length()-1));
 
     }
 
